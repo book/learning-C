7da2d1a2c1f6bac666d342110d87c6cd62785612
Philippe Bruhat (BooK) <book@cpan.org>
2019-01-17 11:25:15 +0100

    Define the hashtable structure, and a constructor

--- 01-hashtable/hashtable.c
+++ 01-hashtable/hashtable.c
@@ -1,5 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct hashtable {
+    int count;
+};
+
+struct hashtable* create_hashtable (void) {
+    struct hashtable *ht = malloc( sizeof( struct hashtable ) );
+    return ht;
+}
+
+int main( int argc, char **argv) { }
+
+/* - create a hashtable */
+  struct hashtable *ht;
+  ht = create_hashtable();
+
+  if( !ht ) { exit(1); }
+
 /*
- * - create a hashtable
  * - check it's empty
  * - add something
  * - get it back
