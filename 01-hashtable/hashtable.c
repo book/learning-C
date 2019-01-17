ec79d9e2bf48333c8e42c20fedc76ef50714ea50
Philippe Bruhat (BooK) <book@cpan.org>
2019-01-17 11:31:35 +0100

    Add size to the hashtable

--- 01-hashtable/hashtable.c
+++ 01-hashtable/hashtable.c
@@ -5,24 +5,40 @@ struct hashtable {
     int count;
 };
 
-struct hashtable* create_hashtable (void) {
-    struct hashtable *ht = malloc( sizeof( struct hashtable ) );
+struct hashtable *create_hashtable(void)
+{
+    struct hashtable *ht = malloc(sizeof(struct hashtable));
+    ht->count = 0;
     return ht;
 }
 
-int main( int argc, char **argv) { }
+int main(int argc, char **argv)
+{
+     int fails = 0;
 
 /* - create a hashtable */
-  struct hashtable *ht;
-  ht = create_hashtable();
+    struct hashtable *ht;
+    ht = create_hashtable();
 
-  if( !ht ) { exit(1); }
+    if (!ht) {
+        exit(1);
+    }
 
+/* - check it's empty */
+    if( ht->count == 0 ) {
+        printf( "ok - hash is empty\n" );
+    }
+    else {
+        printf( "not ok - hash is empty (%d)\n", ht->count );
+        fails++;
+    }
 /*
- * - check it's empty
  * - add something
  * - get it back
  * - delete it
  * - check it's gone
  * - destroy the hashtable
  */
+
+    exit(!!fails);
+}
