3e5693bffcc78c225602a68adb9cb3c71624697c
Philippe Bruhat (BooK) <book@cpan.org>
2019-02-20 21:27:02 +0100

    Add the input and answers to AoC 2015.2

---
+++ AoC/2015/02-two.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int cmp( const void *A, const void *B ) {
+    int a = *( const int * ) A;
+    int b = *( const int * ) B;
+    if ( a < b )
+        return -1;
+    if ( a == b )
+        return 0;
+    return 1;
+}
+
+int main( int argc, char **argv ) {
+    argc = argc;
+    int total = 0;
+    int dim[3];
+    FILE *file = fopen( argv[1], "r" );
+    if ( !file ) {
+        perror( argv[1] );
+        exit( 1 );
+    }
+
+    while ( !feof( file ) ) {
+        fscanf( file, "%dx%dx%d\n", &dim[0], &dim[1], &dim[2] );
+        qsort( dim, 3, sizeof( int ), &cmp );
+        total += 2 * ( dim[0] + dim[1] ) + dim[0] * dim[1] * dim[2];
+    }
+    printf( "%d\n", total );
+    exit( 0 );
+}
