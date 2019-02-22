59f57adeef0e2b3a9ed82d19880b6e3ed1e9d45a
Philippe Bruhat (BooK) <book@cpan.org>
2019-02-23 00:11:17 +0100

    Add solution to the bonus quizz of 2015.5

---
+++ AoC/2015/05-two.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <regex.h>
+
+
+int main( int argc, char **argv ) {
+    argc = argc;
+    FILE *file = fopen( argv[1], "r" );
+    if ( !file ) {
+        perror( argv[1] );
+        exit( 1 );
+    }
+
+    /* compile the regular expressions */
+    char *re_text[] = {
+        "\\([a-z][a-z]\\).*\\1",
+        "\\([a-z]\\).\\1",
+    };
+    regex_t regexp[2];
+
+    for ( int i = 0; i < 2; i++ ) {
+        int re_error = regcomp( &regexp[i], re_text[i], REG_NOSUB );
+        if ( re_error ) {
+            int errbuf_size = regerror( re_error, &regexp[i], NULL, 0 );
+            char *errbuf = malloc( errbuf_size );
+            regerror( re_error, &regexp[i], errbuf, errbuf_size );
+            printf( "%s in %s\n", errbuf, re_text[i] );
+            exit( 1 );
+        }
+    }
+
+    /* read the file */
+    char *line = malloc( 1024 );
+    int nice = 0;
+
+    while ( !feof( file ) ) {
+        fscanf( file, "%s\n", line );
+        if ( !regexec( &regexp[0], line, 0, 0, 0 )
+             && !regexec( &regexp[1], line, 0, 0, 0 ) ) {
+            nice++;
+        }
+    }
+    printf( "%d\n", nice );
+
+    exit( 0 );
+}
