9411081f81003831493e4a9beaeee6fc74ed8b53
Philippe Bruhat (BooK) <book@cpan.org>
2019-12-05 21:53:24 +0100

    2019 day 4 - solution to second puzzle

--- AoC/2019/aoc-04.c
+++ AoC/2019/aoc-04.c
@@ -1,6 +1,7 @@
 #include "aoc.h"
 #include <sys/types.h>
 #include <regex.h>
+
 int main( int argc, char **argv ) {
     int puzzle = 1;
     FILE *file;
@@ -8,12 +9,12 @@ int main( int argc, char **argv ) {
 
     /* compile the regular expressions */
     char *re_text[] = {
-        "\\([0-9]\\)\\1",       // Two adjacent digits are the same( like 22 in 122345 ).
-        "^0*1*2*3*4*5*6*7*8*9*$"        // Going from left to right, the digits never decrease;
+        "^0*1*2*3*4*5*6*7*8*9*$",       // Going from left to right, the digits never decrease;
+        "\\(\\([0-9]\\)\\2\\2*\\)"      // Two adjacent digits are the same( like 22 in 122345 ).
     };
     regex_t regexp[2];
     for ( int i = 0; i < 2; i++ ) {
-        int re_error = regcomp( &regexp[i], re_text[i], REG_NOSUB );
+        int re_error = regcomp( &regexp[i], re_text[i], 0 );
         if ( re_error ) {
             int errbuf_size = regerror( re_error, &regexp[i], NULL, 0 );
             char *errbuf = malloc( errbuf_size );
@@ -23,18 +24,36 @@ int main( int argc, char **argv ) {
         }
     }
 
-    /* read the file */
+    /* read the input */
     int from, to;
     fscanf( file, "%i-%i\n", &from, &to );
 
+    /* test all passwords in the range */
     int valid = 0;
     for ( int i = from; i <= to; i++ ) {
         char *password;
         password = malloc( 7 * sizeof( char ) );
         sprintf( password, "%i", i );
-        if ( !regexec( &regexp[0], password, 0, 0, 0 )
-             && !regexec( &regexp[1], password, 0, 0, 0 ) )
-            valid++;
+
+        regmatch_t pmatch[3];
+        if ( !regexec( &regexp[0], password, 3, pmatch, 0 ) ) {
+            if ( !regexec( &regexp[1], password, 3, pmatch, 0 ) ) {
+                if ( puzzle == 1 ) {
+                    valid++;
+                }
+                else {
+                    // at least one the multiples must be a pair
+                    char *substr = password;
+                    while ( !regexec( &regexp[1], substr, 3, pmatch, 0 ) ) {
+                        if ( pmatch[1].rm_eo - pmatch[1].rm_so == 2 ) {
+                            valid++;
+			    break;
+                        }
+                        substr = &substr[pmatch[1].rm_eo];
+                    }
+                }
+            }
+        }
     }
     printf( "%i\n", valid );
     exit( 0 );
