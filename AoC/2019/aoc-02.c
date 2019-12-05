408233e19af8e4df775997c91f125a18a4edc223
Philippe Bruhat (BooK) <book@cpan.org>
2019-12-05 21:57:48 +0100

    Move the incode computer in its own file

--- AoC/2019/aoc-02.c
+++ AoC/2019/aoc-02.c
@@ -1,34 +1,7 @@
 #include "aoc.h"
+#include "intcode.h"
 #include <string.h>
 #define size 1024
-
-int run( int *source, int val1, int val2 ) {
-    int *program = malloc( size * sizeof( int ) );
-    memcpy( program, source, size );
-    program[1] = val1;
-    program[2] = val2;
-
-    int pc = 0;
-    while ( 1 ) {
-        switch ( program[pc] ) {
-        case 1:
-            program[program[pc + 3]] =
-                program[program[pc + 1]] + program[program[pc + 2]];
-            pc += 4;
-            break;
-        case 2:
-            program[program[pc + 3]] =
-                program[program[pc + 1]] * program[program[pc + 2]];
-            pc += 4;
-            break;
-        case 99:
-            return program[0];
-        default:
-            fprintf( stderr, "Unknown opcode: %i\n", program[pc] );
-            exit( EXIT_FAILURE );
-        }
-    }
-}
 
 int main( int argc, char **argv ) {
     int puzzle = 1;
---
+++ AoC/2019/intcode.h
@@ -0,0 +1,32 @@
+#include <string.h>
+#define size 1024
+
+// the Intcode computer
+
+int run( int *source, int val1, int val2 ) {
+    int *program = malloc( size * sizeof( int ) );
+    memcpy( program, source, size );
+    program[1] = val1;
+    program[2] = val2;
+
+    int pc = 0;
+    while ( 1 ) {
+        switch ( program[pc] ) {
+        case 1:
+            program[program[pc + 3]] =
+                program[program[pc + 1]] + program[program[pc + 2]];
+            pc += 4;
+            break;
+        case 2:
+            program[program[pc + 3]] =
+                program[program[pc + 1]] * program[program[pc + 2]];
+            pc += 4;
+            break;
+        case 99:
+            return program[0];
+        default:
+            fprintf( stderr, "Unknown opcode: %i\n", program[pc] );
+            exit( EXIT_FAILURE );
+        }
+    }
+}
