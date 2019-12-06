d21853541493488e12ca538ec35e092f7946ce0c
Philippe Bruhat (BooK) <book@cpan.org>
2019-12-06 02:03:56 +0100

    2019 day 5 - first puzzle

---
+++ AoC/2019/aoc-05.c
@@ -0,0 +1,24 @@
+#include "aoc.h"
+#include "intcode.h"
+#define size 1024
+
+int main( int argc, char **argv ) {
+    int puzzle = 1;
+    FILE *file;
+    parse_options( argc, argv, 5, &puzzle, &file );
+
+    // read the program in memory
+    int *source = malloc( size * sizeof( int ) );
+    int pc = 0;
+    int Intcode;
+    int res;
+    while ( ( res = fscanf( file, "%i", &Intcode ) ) != EOF ) {
+        source[pc++] = Intcode;
+        res = fscanf( file, "," );
+    }
+
+    // run the program
+    int *program = malloc( size * sizeof( int ) );
+    memcpy( program, source, size );
+    run( program );
+}
--- AoC/2019/intcode.h
+++ AoC/2019/intcode.h
@@ -1,21 +1,43 @@
 #include <string.h>
-#define size 1024
+#define PARAMS 3
+
+int decode( int *mode, int input ) {
+    int exponent = 100;
+    for ( int i = 0; i < PARAMS; i++ ) {
+        mode[i] = input % ( 10 * exponent ) / exponent;
+        exponent *= 10;
+    }
+    return input % 100;
+}
 
 // the Intcode computer
-
 void run( int *program ) {
     int pc = 0;
+    int opcode, input;
+    int mode[PARAMS];
     while ( 1 ) {
-        switch ( program[pc] ) {
+        opcode = decode( mode, program[pc] );
+        switch ( opcode ) {
         case 1:
             program[program[pc + 3]] =
-                program[program[pc + 1]] + program[program[pc + 2]];
+                ( mode[0] ? program[pc + 1] : program[program[pc + 1]] )
+                + ( mode[1] ? program[pc + 2] : program[program[pc + 2]] );
             pc += 4;
             break;
         case 2:
             program[program[pc + 3]] =
-                program[program[pc + 1]] * program[program[pc + 2]];
+                ( mode[0] ? program[pc + 1] : program[program[pc + 1]] )
+                * ( mode[1] ? program[pc + 2] : program[program[pc + 2]] );
             pc += 4;
+            break;
+        case 3:
+            input = 1; // TEST
+            program[program[pc + 1]] = input;
+            pc += 2;
+            break;
+        case 4:
+            printf( "%i\n", mode[0] ? program[pc + 1] : program[program[pc + 1]] );
+            pc += 2;
             break;
         case 99:
             return;
