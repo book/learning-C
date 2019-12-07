5076c41bda0d803b39a520125f6ce84209e4b367
Philippe Bruhat (BooK) <book@cpan.org>
2019-12-07 19:35:20 +0100

    Refactor the computing of arguments for the Intcode computer

--- AoC/2019/intcode.h
+++ AoC/2019/intcode.h
@@ -1,47 +1,48 @@
 #include <string.h>
-#define PARAMS 3
+#define MAX_ARGS 4
 
-int decode( int *mode, int input ) {
+void set_args( int *program, int *args, int pc, int count ) {
     int exponent = 100;
-    for ( int i = 0; i < PARAMS; i++ ) {
-        mode[i] = input % ( 10 * exponent ) / exponent;
+    args[0] = program[pc];
+    for ( int i = 1; i < count; i++ ) {
+        int mode = program[pc] % ( 10 * exponent ) / exponent;
+        args[i] = mode ? program[pc + i]        // immediate mode
+            : program[program[pc + i]]; // position mode
         exponent *= 10;
     }
-    return input % 100;
 }
 
 // the Intcode computer
 void run( int *program, int input ) {
+    int args[MAX_ARGS];         // opcode, arg1, arg2, etc
     int pc = 0;
-    int opcode;
-    int mode[PARAMS];
     while ( 1 ) {
-        opcode = decode( mode, program[pc] );
+        int opcode = program[pc] % 100;
         switch ( opcode ) {
-        case 1:
-            program[program[pc + 3]] =
-                ( mode[0] ? program[pc + 1] : program[program[pc + 1]] )
-                + ( mode[1] ? program[pc + 2] : program[program[pc + 2]] );
+        case 1:                // add
+            set_args( program, args, pc, 4 );
+            program[program[pc + 3]] = args[1] + args[2];
             pc += 4;
             break;
-        case 2:
-            program[program[pc + 3]] =
-                ( mode[0] ? program[pc + 1] : program[program[pc + 1]] )
-                * ( mode[1] ? program[pc + 2] : program[program[pc + 2]] );
+        case 2:                // mul
+            set_args( program, args, pc, 4 );
+            program[program[pc + 3]] = args[1] * args[2];
             pc += 4;
             break;
         case 3:
+            set_args( program, args, pc, 2 );
             program[program[pc + 1]] = input;
             pc += 2;
             break;
         case 4:
-            printf( "%i\n", mode[0] ? program[pc + 1] : program[program[pc + 1]] );
+            set_args( program, args, pc, 2 );
+            printf( "%i\n", args[1] );
             pc += 2;
             break;
         case 99:
             return;
         default:
-            fprintf( stderr, "Unknown opcode: %i\n", program[pc] );
+            fprintf( stderr, "Unknown opcode: %i\n", opcode );
             exit( EXIT_FAILURE );
         }
     }
